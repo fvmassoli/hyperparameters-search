6e979ab643080399ae44bc6eb6f8b0ddcbecee81
fvmass <fabiovaleriomassoli@gmail.com>
2018-12-25 10:49:49 +0100

    Implemented hyperopt search algo

---
+++ main.py
@@ -0,0 +1,196 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+##################
+# Pytorch imports
+##################
+import torch.nn.functional as F
+
+#################
+# Python imports
+#################
+import os
+import time
+import argparse
+import threading
+import numpy as np
+from tqdm import tqdm as tq
+
+###############
+# Tube imports
+###############
+import ray
+from hyperopt import hp
+from ray.tune.suggest import HyperOptSearch
+from ray.tune.schedulers import AsyncHyperBandScheduler
+from ray.tune.util import pin_in_object_store, get_pinned_object
+from ray.tune import Trainable, run_experiments, register_trainable, Experiment
+
+################
+# local imports
+################
+from utils import *
+
+
+pinned_obj_dict = {}
+
+
+class TrainerClass(Trainable):
+    def _setup(self, config):
+        self.data_loader_train = get_pinned_object(pinned_obj_dict['data_loader_train'])
+        self.data_loader_valid = get_pinned_object(pinned_obj_dict['data_loader_valid'])
+        self.model = get_model()
+        self.cuda_available = torch.cuda.is_available()
+        print("Cuda is available: {}".format(self.cuda_available))
+        opt = getattr(torch.optim, self.config['optimizer'])
+        self.optimizer = opt(self.model.classifier[6].parameters(), lr=self.config['lr'])
+        self.batch_accumulation = self.config['batch_accumulation']
+
+    def _train_iter(self):
+        j = 1
+        i = 0
+        self.model.train()
+        self.optimizer.zero_grad()
+        progress_bar = tq(self.data_loader_train)
+        progress_bar.set_description("Training")
+        avg_loss = 0.0
+        for batch_idx, (data, target) in enumerate(progress_bar, 1):
+            if self.cuda_available:
+                data = data.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+            output = self.model(data)
+            loss = F.cross_entropy(output, target)
+            loss.backward()
+            if j % self.batch_accumulation == 0:
+                j = 1
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+            else:
+                j += 1
+            if i == (len(self.data_loader_train) / 2): break
+            i += 1
+            avg_loss += loss.item()
+            if batch_idx % (len(self.data_loader_train) / 8) == 0:
+                progress_bar.set_postfix({'Loss': '{:.3f}'.format((avg_loss / (batch_idx * target.shape[0])))})
+        torch.cuda.empty_cache()
+
+    def _valid(self):
+        self.model.eval()
+        avg_loss = 0.0
+        avg_acc = 0.0
+        progress_bar = tq(self.data_loader_valid)
+        progress_bar.set_description("Validation")
+        for batch_idx, (data, target) in enumerate(progress_bar, 1):
+            if self.cuda_available:
+                data = data.cuda(non_blocking=True)
+                target = target.cuda(non_blocking=True)
+            output = self.model(data)
+            loss = F.cross_entropy(output, target)
+            avg_loss += loss
+            y_hat = output.argmax(dim=1)
+            avg_acc += (target == y_hat).sum().item()
+            if batch_idx % (len(self.data_loader_valid) / 8) == 0:
+                l = avg_loss / (batch_idx * target.shape[0])
+                acc = avg_acc / (batch_idx * target.shape[0])
+                metrics = {
+                    'loss': '{:.3f}'.format(l.item()),
+                    'acc': '{:.2f}%'.format(acc)
+                }
+                progress_bar.set_postfix(metrics)
+        loss = avg_loss / len(self.data_loader_valid.dataset)
+        acc = avg_acc / len(self.data_loader_valid.dataset)
+        torch.cuda.empty_cache()
+        return {"loss": loss.detach().cpu().item(), "acc": acc}
+
+    def _train(self):
+        self._train_iter()
+        return self._valid()
+
+    def _save(self, checkpoint_dir):
+        checkpoint_path = os.path.join(checkpoint_dir, "model.pth")
+        torch.save(self.model.state_dict(), checkpoint_path)
+        return checkpoint_path
+
+    def _restore(self, checkpoint_path):
+        self.model.load_state_dict(checkpoint_path)
+
+
+def main(args):
+
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+
+    ray.init()
+
+    t_loader, v_loader = get_loaders(train_batch_size=16)
+    pinned_obj_dict['data_loader_train'] = pin_in_object_store(t_loader)
+    pinned_obj_dict['data_loader_valid'] = pin_in_object_store(v_loader)
+
+    trainable_name = 'hyp_search_train'
+    register_trainable(trainable_name, TrainerClass)
+
+    reward_attr = "acc"
+
+    #############################
+    # Define hyperband scheduler
+    #############################
+    hpb = AsyncHyperBandScheduler(time_attr="training_iteration",
+                                  reward_attr=reward_attr,
+                                  grace_period=40,
+                                  max_t=300)
+
+    ##############################
+    # Define hyperopt search algo
+    ##############################
+    space = {
+        'lr': hp.uniform('lr', 0.001, 0.1),
+        'optimizer': hp.choice("optimizer", ['Adam', 'SGD', 'Adadelta']),
+        'batch_accumulation': hp.choice("batch_accumulation", [4, 8, 16])
+    }
+    hos = HyperOptSearch(space, max_concurrent=4, reward_attr=reward_attr)
+
+    #####################
+    # Define experiments
+    #####################
+    exp_name = "hyp_search_hyperband_hyperopt_{}".format(time.strftime("%Y-%m-%d_%H.%M.%S"))
+    exp = Experiment(
+        name=exp_name,
+        run=trainable_name,
+        num_samples=10,  # the number of experiments
+        trial_resources={
+            "cpu": args.numCpu,
+            "gpu": args.numGpu
+        },
+        checkpoint_freq=3,
+        checkpoint_at_end=True,
+        stop={
+            reward_attr: 0.95,
+            "training_iteration": 9,  # how many times a specific config will be trained
+        }
+    )
+
+    ##################
+    # Run tensorboard
+    ##################
+    if args.runTensorBoard:
+        thread = threading.Thread(target=launch_tensorboard, args=[exp_name])
+        thread.start()
+        launch_tensorboard(exp_name)
+
+    ##################
+    # Run experiments
+    ##################
+    run_experiments(exp, search_alg=hos, scheduler=hpb, verbose=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-s', '--seed', type=int, default=41, help='Set the randome generators seed (default: 41)')
+    parser.add_argument('-c', '--numCpu', type=int, default=4, help='Set the number of CPU to use (default: 4)')
+    parser.add_argument('-g', '--numGpu', type=int, default=0, help='Set the number of GPU to use (default: 0)')
+    parser.add_argument('-t', '--runTensorBoard', action='store_true', help='Run tensorboard (default: false)')
+    args = parser.parse_args()
+    main(args)
---
+++ utils.py
@@ -0,0 +1,93 @@
+##################
+# Pytorch imports
+##################
+import torch
+import torch.nn as nn
+import torchvision.transforms as t
+import torchvision.models as models
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
+from torch.utils.data.sampler import WeightedRandomSampler
+
+
+def make_weights_for_balanced_classes(images, n_classes):
+    """
+    Since the classes are not balanced let's create weights for them
+
+    """
+    count = [0] * n_classes
+    for item in images:
+        count[item[1]] += 1
+    weight_per_class = [0.] * n_classes
+    N = float(sum(count))
+    for i in range(n_classes):
+        weight_per_class[i] = N / float(count[i])
+    weights = [0] * len(images)
+    for idx, val in enumerate(images):
+        weights[idx] = weight_per_class[val[1]]
+    return weights
+
+
+def get_transforms(train, hf=0.5, rr_degrees=30, gs=0.3,
+                   mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
+    if train:
+        return t.Compose([
+            t.Resize(256),
+            t.RandomHorizontalFlip(p=hf),
+            t.RandomRotation(degrees=rr_degrees),
+            t.RandomGrayscale(p=gs),
+            t.RandomCrop(224),
+            t.ToTensor(),
+            t.Normalize(mean=mean, std=std),
+        ])
+    else:
+        return t.Compose([
+            t.Resize(256),
+            t.CenterCrop(224),
+            t.ToTensor(),
+            t.Normalize(mean=mean, std=std),
+        ])
+
+
+def get_loaders(train_batch_size):
+    print("Loading data...")
+    cuda_available = torch.cuda.is_available()
+    data_dir = '/media/fabiovalerio/Data/facebook_pytorch_challenge/pytorch_challenge/flower_data'
+    train_dir = data_dir + '/train'
+    valid_dir = data_dir + '/valid'
+    train_data_set = ImageFolder(root=train_dir, transform=get_transforms(train=True))
+    valid_data_set = ImageFolder(root=valid_dir, transform=get_transforms(train=False))
+    weights = make_weights_for_balanced_classes(train_data_set.imgs, len(train_data_set.classes))
+    weights = torch.DoubleTensor(weights)
+    sampler = WeightedRandomSampler(weights, len(weights))
+    train_data_loader = DataLoader(dataset=train_data_set,
+                                   sampler=sampler,
+                                   batch_size=train_batch_size,
+                                   num_workers=4,
+                                   pin_memory=cuda_available)
+    valid_data_loader = DataLoader(dataset=valid_data_set,
+                                   batch_size=8,
+                                   num_workers=4,
+                                   pin_memory=cuda_available)
+    print("Data loaded!!!")
+    return train_data_loader, valid_data_loader
+
+
+def get_model():
+    print("Loading model...")
+    model = models.vgg16(pretrained=True)
+    for n, p in model.named_parameters():
+        p.requires_grad = False
+    model.classifier[6] = nn.Linear(in_features=4096, out_features=102, bias=True)
+    nn.init.xavier_normal_(model.classifier[6].weight)
+    model.classifier[6].bias.data.fill_(0)
+    model.classifier[6].weight.requires_grad = True
+    model.classifier[6].bias.requires_grad = True
+    print("Model loaded!!!")
+    return model
+
+
+def launch_tensorboard(logdir):
+    import os
+    os.system('tensorboard --logdir=~/ray_results/' + logdir)
+    return
